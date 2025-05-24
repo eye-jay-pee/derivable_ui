657d5f2bb9453a8685421bdf8efcca880a2a0a89
Isaac Peterson <isaacpeterson2017@gmail.com>
2025-05-24 17:37:12 -0500

    viewer and editor traits defined
    and implemented in example struct, person

--- src/main.rs
+++ src/main.rs
@@ -19,45 +19,110 @@ mod people {
             }
         }
     }
+    pub mod widgets {
+        use super::Person;
+        use eframe::egui::{Response, Ui, Widget};
+
+        pub mod viewer {
+            use super::*;
+            use crate::widgets::Viewable;
+            pub struct PersonViewer<'a> {
+                pub data: &'a Person,
+            }
+            impl<'a> Widget for PersonViewer<'a> {
+                fn ui(self, ui: &mut Ui) -> Response {
+                    ui.label(self.data.to_string())
+                }
+            }
+            impl Viewable for Person {
+                type Viewer<'a> = PersonViewer<'a>;
+                fn get_viewer(&self) -> Self::Viewer<'_> {
+                    PersonViewer { data: self }
+                }
+            }
+        }
+
+        pub mod editor {
+            use super::*;
+            use crate::widgets::Editable;
+            pub struct PersonEditor<'a> {
+                pub data: &'a mut Person,
+            }
+            impl<'a> Widget for PersonEditor<'a> {
+                fn ui(self, ui: &mut Ui) -> Response {
+                    ui.vertical(|ui| {
+                        ui.label("given name");
+                        ui.text_edit_singleline(&mut self.data.first);
+                        ui.label("family name");
+                        ui.text_edit_singleline(&mut self.data.last);
+                        ui.label("age");
+                    })
+                    .response
+                }
+            }
+            impl Editable for Person {
+                type Editor<'a> = PersonEditor<'a>;
+                fn get_editor(&mut self) -> Self::Editor<'_> {
+                    PersonEditor { data: self }
+                }
+            }
+        }
+    }
 }
 use people::Person;
 
 mod widgets {
-    use eframe::{AppCreator, NativeOptions};
+    use eframe::{
+        egui::{CentralPanel, Context, Widget},
+        run_native, App, AppCreator, Error, Frame, NativeOptions,
+    };
 
-    pub struct TopLevel<'a, T: std::fmt::Display> {
+    pub trait Viewable: std::fmt::Display {
+        type Viewer<'a>: Widget + 'a
+        where
+            Self: 'a;
+
+        fn get_viewer(&self) -> Self::Viewer<'_>;
+    }
+    pub trait Editable: Viewable {
+        type Editor<'a>: Widget + 'a
+        where
+            Self: 'a;
+
+        fn get_editor(&mut self) -> Self::Editor<'_>;
+    }
+
+    pub struct TopLevel<'a, T: Editable> {
         pub data: &'a mut T,
     }
 
-    impl<'a, T: std::fmt::Display + 'a> eframe::App for TopLevel<'a, T> {
-        fn update(
-            &mut self,
-            ctx: &eframe::egui::Context,
-            _frame: &mut eframe::Frame,
-        ) {
-            eframe::egui::CentralPanel::default().show(ctx, |ui| {
-                ui.label(self.data.to_string());
+    impl<'a, T: Editable + 'a> App for TopLevel<'a, T> {
+        fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+            CentralPanel::default().show(ctx, |ui| {
+                ui.add(self.data.get_viewer());
+                ui.add(self.data.get_editor());
             });
         }
     }
 
-    impl<'a, T: std::fmt::Display + 'a> TopLevel<'a, T> {
+    impl<'a, T: Editable + 'a> TopLevel<'a, T> {
         pub fn new(data: &'a mut T) -> Self {
             TopLevel { data }
         }
 
-        pub fn launch(self) -> Result<(), eframe::Error> {
+        pub fn launch(self) -> Result<(), Error> {
             let app_title = std::any::type_name::<T>();
             let app_options = NativeOptions::default();
             let app_creator: AppCreator =
                 Box::new(move |_cc| Ok(Box::new(self)));
 
-            eframe::run_native(app_title, app_options, app_creator)
+            run_native(app_title, app_options, app_creator)
         }
     }
 }
 use widgets::TopLevel;
-
+pub use widgets::{Editable, Viewable};
 fn main() -> Result<(), eframe::Error> {
-    TopLevel::new(&mut Person::new("McLovin", "", 21)).launch()
+    let mut person = Person::new("McLovin", "", 21);
+    TopLevel::new(&mut person).launch()
 }
